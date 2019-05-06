7497c37329b246259e607a09b6950cc15738b1b4
Ali Naderlou <alinaderlo77@hotmail.com>
2019-05-06 19:29:04 +0430

    Profile-Info component added

---
+++ server/client/src/components/profile-info.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './profile-info.sass';
+function Profile_Info() {
+  return (
+    <section className="profile-info-container">
+      <div className="user-image-container">
+        <img src="images/profile.jpg" alt="Ali Naderlou" />
+      </div>
+      <div className="user-about-container">
+        <p>
+          Amet anim fugiat cillum tempor dolore dolor quis qui aliquip laboris
+          consequat commodo ea consequat.
+        </p>
+      </div>
+      <div className="user-social-medias-container">
+        <ul>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Linkedin</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Profile_Info;
