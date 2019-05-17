1dcaf7024ca2ee0f7fb91350cceb5f2f8ffdfec9
Ali Naderlou <alinaderlo77@hotmail.com>
2019-05-18 02:07:59 +0430

    Slider v0.1

--- server/client/src/components/work-sample.js
+++ server/client/src/components/work-sample.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 import './styles/work-sample.scss';
 import MediaContent from './media-content';
 function WorkSample(props) {
   return (
     <div className="work-sample box-shadow">
       <section className="work-sample-info-container">
-        <MediaContent
-          media={props.medias[0]}
-          mediasCount={5}
-          currentMedia={0}
-        />
+        <AwesomeSlider className="aws-btn">
+          <div data-src="/images/1.jpg" />
+          <div data-src="/images/722598.jpg" />
+          <div data-src="/videos/1.mp4" />
+        </AwesomeSlider>
         <div className="title">
           <h3>{props.title}</h3>
         </div>
