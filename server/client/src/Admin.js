287aab7820e4ad30d086f6ddd56283385503a08e
Ali Naderlou <alinaderlo77@hotmail.com>
2019-06-24 02:16:33 +0430

    Login Added v0.1

---
+++ server/client/src/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Admin extends React.Component {
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state={
+      workSamples: [],
+    };
+  }
+  render() {
+    return <div>fuck</div>;
+  }
+}
+export default Admin
--- server/client/src/App.js
+++ server/client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Client from './Client';
 import Login from './Login';
-
+import withAuth from './withAuth';
+import Admin from './Admin'
 class App extends Component {
   constructor(props) {
     super();
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Client} />
           <Route path="/itsme" component={Login} />
+          <Route path="/admin" component={withAuth(Admin)} />
         </Switch>
       </BrowserRouter>
     );
