2b629e50aae112d079b854d656feef559b33e29d
Ali Naderlou <alinaderlo77@hotmail.com>
2019-07-05 13:56:41 +0430

    New Db Address

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let db = mongoose.connect('mongodb://localhost:27017/AliBlog', {
+let db = mongoose.connect('uwdn3f3vvfzgr6j6xgc4:Aw5dyQWggTrtt2CweSUq@bxjsbneijuv151o-mongodb.services.clever-cloud.com/bxjsbneijuv151o', {
   useNewUrlParser: true,
   useFindAndModify:false,
   useCreateIndex:true
