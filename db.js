0b483bb306fa95b58ae511c5d0e4c951b47e953a
Ali Naderlou <alinaderlo77@hotmail.com>
2019-07-05 14:44:52 +0430

    change mongo addr

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let db = mongoose.connect('uwdn3f3vvfzgr6j6xgc4:Aw5dyQWggTrtt2CweSUq@bxjsbneijuv151o-mongodb.services.clever-cloud.com/bxjsbneijuv151o', {
+let db = mongoose.connect('uwdn3f3vvfzgr6j6xgc4:Aw5dyQWggTrtt2CweSUq@mongodb://uwdn3f3vvfzgr6j6xgc4:Aw5dyQWggTrtt2CweSUq@bxjsbneijuv151o-mongodb.services.clever-cloud.com:27017/bxjsbneijuv151o', {
   useNewUrlParser: true,
   useFindAndModify:false,
   useCreateIndex:true
